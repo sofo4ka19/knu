44eb22391b17870ff0ff152731b929a589c1ad75
Sofiia Broiako <yroha0013@gmail.com>
2024-03-29 14:36:31 +0200

    update demo mode

--- 2-semester/lab3_sort/a/main.cpp
+++ 2-semester/lab3_sort/a/main.cpp
@@ -32,41 +32,11 @@ int lomuto(std::vector<double>& array, int low, int high, bool show){
 }
 void quickSort(std::vector<double>& array, int low, int high, bool show){
     if(low<high){
-        //if(show) print(array);
         int pivotNum = lomuto(array,low, high, show);
         quickSort(array, low, pivotNum-1, show);
         quickSort(array, pivotNum+1, high, show);
     }
 }
-/*void mergeSort(std::vector<double>& array, int low, int high, bool show){
-    int mid = low + (high - low)/2;
-    if(mid-low>=1) mergeSort(array, low, mid, show);
-    if(high - (mid+1) >= 1) mergeSort(array, mid+1, high, show);
-
-    int i=0, j=mid+1, k=low;
-    std::vector<double> temp;
-    for(int m=low; m<=mid; m++){
-        temp.push_back(array[m]);
-    }
-    while(i < temp.size() && j<=high){
-        if(temp[i] >= array[j]){
-            array[k] = temp[i];
-            i++;
-            k++;
-        }
-        else{
-            array[k] = array[j];
-            j++;
-            k++;
-        }
-    }
-    while (i<temp.size()){
-        array[k] = temp[i];
-        i++;
-        k++;
-    }
-    if(show) print(array);
-}*/
 void merge(std::vector<double>& array, int low, int mid, int high, bool show) {
     int i = 0, j = mid + 1, k = low;
     std::vector<double> temp;
@@ -111,9 +81,6 @@ void combineSort(std::vector<double>& array, int n, int low, int high, bool show
     else if (size <= n)
     {
         bubbleSort(array,low, high, show);
-        std::cout << low << " " << high << std::endl;
-        print(array);
-        std::cout << "we have sorted elements from " << array[low] << " to " << array[high] << std::endl;
     }
     else
     {
@@ -128,32 +95,58 @@ void create(std::vector<double>& right, std::vector<double>& wrong, std::vector<
     for(int i=0; i<size; i++){
         right.push_back(size-i);
         wrong.push_back(i);
-        mix.push_back(rand()/100);
+        mix.push_back(rand()/100.0);
     }
     std::swap(right[rand()%size],right[rand()%size]);
     std::swap(wrong[rand()%size],wrong[rand()%size]);
 }
 void demo(int size){
-    std::vector<double> right, wrong, mix;
-    create(right, wrong, mix, 25);
-    std::cout << "actions for array in right order" << std::endl;
+    std::vector<double> right, wrong, mix, current;
+    create(right, wrong, mix, size);
+    std::cout << "actions for array in right order:" << std::endl;
+    print(right);
+    current=right;
+    std::cout << "bubble sort:" << std::endl;
+    bubbleSort(current, 0, size-1, true);
+    current=right;
+    std::cout << "quick sort (lomuto):" << std::endl;
+    quickSort(current, 0, size-1, true);
+    current=right;
+    std::cout << "merge sort:" << std::endl;
+    (mergeSort(current, 0, size-1, true));
+    current=right;
+    std::cout << "combine sort:" << std::endl;
+    (combineSort(current, 10, 0,size-1, true));
+
+    std::cout << "actions for array in wrong order:" << std::endl;
+    print(wrong);
+    current=wrong;
+    std::cout << "bubble sort:" << std::endl;
+    bubbleSort(current, 0, size-1, true);
+    current=wrong;
+    std::cout << "quick sort (lomuto):" << std::endl;
+    quickSort(current, 0, size-1, true);
+    current=wrong;
+    std::cout << "merge sort:" << std::endl;
+    (mergeSort(current, 0, size-1, true));
+    current=wrong;
+    std::cout << "combine sort:" << std::endl;
+    (combineSort(current, 10, 0,size-1, true));
+
+    std::cout << "actions for array in mixed order:" << std::endl;
     print(mix);
-    bubbleSort(mix, 0, 24, true);
-    //quickSort(mix, 0, size-1, true);
-    //(mergeSort(mix, 0, size-1, true));
-    //(combineSort(mix, 10, 0,24, true));
-
-    /*print(array);
-    print(bubbleSort(array, 25));
-    print(quickSort(array, 0, 24));
-    print(mergeSort(array, 0, 24));
-    print(combineSort(array, 10, 0,24));
-
-    print(array);
-    print(bubbleSort(array, 25));
-    print(quickSort(array, 0, 24));
-    print(mergeSort(array, 0, 24));
-    print(combineSort(array, 10, 0,24));*/
+    current=mix;
+    std::cout << "bubble sort:" << std::endl;
+    bubbleSort(current, 0, size-1, true);
+    current=mix;
+    std::cout << "quick sort (lomuto):" << std::endl;
+    quickSort(current, 0, size-1, true);
+    current=mix;
+    std::cout << "merge sort:" << std::endl;
+    (mergeSort(current, 0, size-1, true));
+    current=mix;
+    std::cout << "combine sort:" << std::endl;
+    (combineSort(current, 10, 0,size-1, true));
 }
 int main() {
     demo(25);
