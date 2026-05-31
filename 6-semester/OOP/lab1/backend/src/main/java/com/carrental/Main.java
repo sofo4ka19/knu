46083bb2b172539797576b105836fe28ebd9741f
Sofiia Broiako <yroha0013@gmail.com>
2026-05-31 21:36:39 +0300

    add entry point

--- 6-semester/OOP/lab1/backend/src/main/java/com/carrental/Main.java
+++ 6-semester/OOP/lab1/backend/src/main/java/com/carrental/Main.java
@@ -1,17 +1,72 @@
 package com.carrental;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.tomcat.util.descriptor.web.FilterDef;
+import org.apache.tomcat.util.descriptor.web.FilterMap;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.File;
+
+public class Main {
+
+    private static final Logger log = LogManager.getLogger(Main.class);
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(PORT);
+        tomcat.setBaseDir(System.getProperty("java.io.tmpdir"));
+
+        Context ctx = tomcat.addContext("", new File(".").getAbsolutePath());
+
+        registerServlets(tomcat, ctx);
+        registerFilters(ctx);
+
+        tomcat.getConnector();
+
+        tomcat.start();
+        log.info("Server started on http://localhost:{}", PORT);
+        tomcat.getServer().await();
+    }
+
+    private static void registerServlets(Tomcat tomcat, Context ctx) {
+        Tomcat.addServlet(ctx, "CarServlet",
+                new com.carrental.servlet.CarServlet());
+        ctx.addServletMappingDecoded("/api/cars/*", "CarServlet");
+
+        Tomcat.addServlet(ctx, "OrderServlet",
+                new com.carrental.servlet.OrderServlet());
+        ctx.addServletMappingDecoded("/api/orders/*", "OrderServlet");
+
+        Tomcat.addServlet(ctx, "AdminServlet",
+                new com.carrental.servlet.AdminServlet());
+        ctx.addServletMappingDecoded("/api/admin/*", "AdminServlet");
+    }
+
+    private static void registerFilters(Context ctx) {
+        addFilter(ctx, "CorsFilter",
+                "com.carrental.filter.CorsFilter", "/*");
+
+        addFilter(ctx, "AuthFilter",
+                "com.carrental.filter.AuthFilter", "/api/*");
+
+        addFilter(ctx, "RoleFilter",
+                "com.carrental.filter.RoleFilter", "/api/admin/*");
+    }
+
+    private static void addFilter(Context ctx, String filterName,
+                                  String filterClassName, String urlPattern) {
+        FilterDef filterDef = new FilterDef();
+        filterDef.setFilterName(filterName);
+        filterDef.setFilterClass(filterClassName);
+        ctx.addFilterDef(filterDef);
+
+        FilterMap filterMap = new FilterMap();
+        filterMap.setFilterName(filterName);
+        filterMap.addURLPattern(urlPattern);
+        ctx.addFilterMap(filterMap);
     }
 }
